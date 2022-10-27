e215e000dee533993ed26c8e40497edb3f1531dc
Joy Goswami <joy63586@gmail.com>
2022-10-27 14:18:49 +0600

    01

---
+++ app.js
@@ -0,0 +1,9 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const alertEl = document.querySelector(".alert");
+const navListEl = document.querySelector(".nav-list");
+//const btnEl = document.querySelector(".btn");
+
+// hamburgerMenu functionality
+hamburgerMenu.addEventListener("click", () => {
+  navListEl.classList.toggle("nav-show");
+});
